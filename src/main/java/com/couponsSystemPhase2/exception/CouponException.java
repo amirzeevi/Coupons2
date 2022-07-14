f584b075b0d895ab397be15d9120f45325cab387
amirzeevi <94979040+amirzeevi@users.noreply.github.com>
2022-07-14 11:40:44 +0300

    -

--- src/main/java/com/couponsSystemPhase2/exception/CouponException.java
+++ src/main/java/com/couponsSystemPhase2/exception/CouponException.java
@@ -1,4 +1,4 @@
-package com.couponsSystemPhase2.exception;
+package com.couponsSystemPhase2.exception; 
 
 public class CouponException extends RuntimeException{
     public CouponException(String message) {
